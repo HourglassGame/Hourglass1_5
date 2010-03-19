4016f46c177186208a7d9be07b5e62000e5a6597
Evan Wallace <onlyone@senet.com.au>
2010-03-19 08:19:32 +0000

    Missed renaming one!

---
+++ Editor/Editor_ImageLoader.cpp
@@ -0,0 +1,46 @@
+/*
+    ------------===constructor options===---------------
+    
+    ImageLoader()                 - selects game resource directory
+    ImageLoader(ImagePathType)    - switches between known resource directories
+    ImageLoader(std::string)       - selects custom relative resource directory
+    
+    -------------===variable names===-------------------
+    
+    /resources/images/                   - relativePath
+    box.bmp                              - fileName
+    C:\...                               - exePath
+    C:\.../resources/images/             - absolutePath
+    C:\.../resources/images/box.bmp      - filePath
+*/
+#include "Editor_ImageLoader.h" // class's header file
+extern int MAX_PATH;
+std::map<std::string,BITMAP*> ImageLoader::loadedImages;
+ImageLoader::ImageLoader(const ImagePathType) 
+{
+}
+
+ImageLoader::ImageLoader(const std::string) 
+{
+}
+
+// class destructor
+ImageLoader::~ImageLoader()
+{
+}
+
+BITMAP* ImageLoader::LoadImage(const std::string fileName)
+{
+	std::string filePath;
+    filePath = pathToResourceDirectory + fileName;
+	if (loadedImages.find(filePath) == loadedImages.end()) {
+		BITMAP* newBitmap = load_bitmap(filePath.c_str(), NULL);
+		if (!newBitmap)
+		{
+			//const char whichImage = fileName.data();
+			throw ImageNotLoadedException(fileName);//fileName.data());
+		}
+		loadedImages[filePath] = newBitmap;
+	}
+	return loadedImages[filePath];
+}
